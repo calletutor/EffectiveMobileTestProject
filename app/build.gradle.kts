a9a2c131ee6366cf5be0698c9496e24830d711fd
German <calletutor@gmail.com>
2026-03-13 09:36:13 +0300

    поменял bottom navigation bar на стандрартный

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation(libs.koin.androidx.compose)
     implementation(libs.adapter.delegates)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
